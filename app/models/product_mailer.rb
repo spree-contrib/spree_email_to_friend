4d6f2d3e4f58b76dc5915ad67cd353a64cfc7c05
Jorge Calás Lozano <calas@qvitta.net>
2009-10-15 01:39:07 +0200

    Sending recommendations from default user and reply_to sender's address

--- app/models/product_mailer.rb
+++ app/models/product_mailer.rb
@@ -3,7 +3,8 @@ class ProductMailer < ActionMailer::Base
 
   def mail_to_friend(product, mail)
     subject       I18n.t('email_to_friend.you_would_like_this')
-    from          mail.sender_email
+    from          Spree::Config[:mails_from]
+    reply_to      mail.sender_email
     recipients    mail.recipient_email
     sent_on       Time.now
     body          :product => product, :mail => mail
