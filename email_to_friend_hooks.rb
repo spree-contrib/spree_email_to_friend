5554e66b85b902594466d13415e0d378cdeeed79
David North <davidnorth@railsdog.com>
2009-12-17 21:23:00 +0000

    update hooks to new erb style

--- email_to_friend_hooks.rb
+++ email_to_friend_hooks.rb
@@ -1,5 +1,7 @@
 class EmailToFriendHooks < Spree::ThemeSupport::HookListener
   insert_after :product_description do
-    content_tag('p', link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('product', @product)), :id => 'email_to_friend')
+    %(<p class="email_to_friend">
+        <%= link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('product', @product)) %>
+    </p>)
   end
 end
