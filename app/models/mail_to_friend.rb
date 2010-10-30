b8256a0c09ed0261a953bdc908b9b175911c9320
Brian Quinn <briandquinn@gmail.com>
2010-10-30 15:01:05 +0100

    Improved email validation

--- app/models/mail_to_friend.rb
+++ app/models/mail_to_friend.rb
@@ -5,17 +5,17 @@ class MailToFriend
 
   validates :sender_name, :presence => true
   validates :recipient_name, :presence => true
-  validates :sender_email, :format => { :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i }
-  validates :recipient_email, :format => { :with => /^([A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}[,;]?\s?)+$/i }
+  validates :sender_email, :format => { :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,8}$/i }
+  validates :recipient_email, :format => { :with => /^([-a-z0-9_+\.]+\@[-a-z0-9]+\.+[a-z0-9]{2,8}[,;]*)+$/i , :message => "contains invalid email address" }
 
   def initialize(opts = {})
     @sender_email = opts[:sender_email] || ' '
     @sender_name  = opts[:sender_name]  || @sender_email.split('@', 2)[0].titleize
-    @recipient_email = (opts[:recipient_email] || ' ').gsub(';', ',')
+    @recipient_email = (opts[:recipient_email] || ' ').gsub(';', ',').gsub(/\s/ , "")
     @recipient_name = opts[:recipient_name] || @recipient_email.split('@', 2)[0].titleize
     @message = opts[:message]
   end
-  
+
   def persisted?
     false
   end
