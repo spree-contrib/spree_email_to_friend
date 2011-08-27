07679230ebe8830dbb08a0804940d22ec02a7d16
Trung Lê <joneslee85@gmail.com>
2011-08-27 17:32:52 +1000

    Refactor work

---
+++ app/overrides/add_captcha_settings_to_admin_configurations_menu.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "admin/configurations/index",
+                     :name => "converted_admin_configurations_menu_384123912",
+                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                     :text => "<%= configurations_menu_item(I18n.t('captcha.captcha_settings'), admin_captcha_settings_path, I18n.t('captcha.manage_keys')) %>",
+                     :disabled => false)
