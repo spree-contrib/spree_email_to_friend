41f961a0ee337551715f084819891cb454f06c0b
Rodrigo Rosalin <rodrigorosalin2@yahoo.com.br>
2011-10-16 19:29:39 -0200

    corrected method call to deliver

--- app/controllers/email_sender_controller.rb
+++ app/controllers/email_sender_controller.rb
@@ -36,7 +36,7 @@ class EmailSenderController < Spree::BaseController
 
   #extract send message to make easier to override
   def send_message(object, mail_to_friend)
-    ToFriendMailer.deliver_mail_to_friend(object,@mail_to_friend)
+    ToFriendMailer.mail_to_friend(object,@mail_to_friend).deliver
   end
 
   def find_object
