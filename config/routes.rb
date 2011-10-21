265600ec4e6d2f39996a585d159067bc348d91e7
Trung Lê <joneslee85@gmail.com>
2011-10-22 10:03:17 +1100

    Application routes should be engine routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
-Rails.application.routes.draw do
-
-  match "/email_to_friend/:type/:id" => 'email_sender#send_mail', :as => :email_to_friend
+SpreeEmailToFriend::Engine.routes.draw do
+  match '/email_to_friend/:type/:id' => 'email_sender#send_mail', :as => :email_to_friend
 
   namespace :admin do
     resource :captcha_settings
   end
-
 end
