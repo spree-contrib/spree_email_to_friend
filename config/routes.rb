aa68252f46eebe63243ea417b69f1bc1cb149d10
Jeff Dutil <jdutil21@gmail.com>
2011-10-24 03:46:15 -0400

    Revert "Application routes should be engine routes"
    This reverts commit 265600ec4e6d2f39996a585d159067bc348d91e7.
    
    @joneslee85 the routes should remain Application routes otherwise they are not available to the Host Application.
    
    The change to engine routes breaks the extension resulting in undefined_method email_to_friend_url errors rendering the override.
    
    I think it makes more sense to leave the routes as application routes in order to avoid having users mount the the engine in the host applications route configuration, unless you know a way we could do that as a railtie?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-SpreeEmailToFriend::Engine.routes.draw do
-  match '/email_to_friend/:type/:id' => 'email_sender#send_mail', :as => :email_to_friend
+Rails.application.routes.draw do
+
+  match "/email_to_friend/:type/:id" => 'email_sender#send_mail', :as => :email_to_friend
 
   namespace :admin do
     resource :captcha_settings
   end
+
 end
