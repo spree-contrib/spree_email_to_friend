8c63623268fb5722db3b5b210036769805f1bdf5
John Dyer <lbrapid@gmail.com>
2011-11-16 11:16:51 -0500

    Fix broken deface overrides

--- app/overrides/add_captcha_settings_to_admin_configurations_menu.rb
+++ app/overrides/add_captcha_settings_to_admin_configurations_menu.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "admin/configurations/index",
+Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
                      :name => "converted_admin_configurations_menu_384123912",
                      :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                     :text => "<%= configurations_menu_item(I18n.t('captcha.captcha_settings'), admin_captcha_settings_path, I18n.t('captcha.manage_keys')) %>",
+                     :text => "<%= configurations_menu_item(I18n.t('captcha.captcha_settings'), main_app.admin_captcha_settings_path, I18n.t('captcha.manage_keys')) %>",
                      :disabled => false)
--- lib/spree_email_to_friend/engine.rb
+++ lib/spree_email_to_friend/engine.rb
@@ -11,9 +11,9 @@ module SpreeEmailToFriend
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
 
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
-      end
+      #Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
+      #  Rails.application.config.cache_classes ? require(c) : load(c)
+      #end
     end
 
     config.to_prepare &method(:activate).to_proc
