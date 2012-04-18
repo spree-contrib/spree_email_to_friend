5a21f5960511c12a0f19f23432b3e9c1b2ca2b16
Greg Reinacker <gregr@rassoc.com>
2012-04-18 13:22:40 -0600

    change :mails_from preference

--- app/mailers/spree/to_friend_mailer.rb
+++ app/mailers/spree/to_friend_mailer.rb
@@ -1,5 +1,5 @@
 class Spree::ToFriendMailer < ActionMailer::Base
-  default :from => Spree::Config[:mails_from]
+  default :from => Spree::MailMethod.current.preferred_mails_from
 
   def mail_to_friend(object, mail)
     @object = object
