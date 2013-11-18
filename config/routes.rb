db92c2404f442273975a4d4b89126c56688cdbff
Richard Hart <richard@ur-ban.com>
2013-11-18 14:54:45 +0000

    email_to_friend route now accepts get/post
    Fixes #41

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Spree::Core::Engine.routes.prepend do
 
-  match "/email_to_friend/:type/:id" => 'email_sender#send_mail', :as => :email_to_friend
+  match "/email_to_friend/:type/:id" => 'email_sender#send_mail', as: :email_to_friend, via: [:get, :post]
 
   namespace :admin do
     resource :captcha_settings
   end
-
 end
