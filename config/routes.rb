fe0a738f5cccfabf42294fe56b7ba4a51e2fc8d3
Richard Hart <richard@ur-ban.com>
2013-11-18 14:54:45 +0000

    email_to_friend route now accepts get/post
    Fixes #41
    
    Conflicts:
    	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
 
-  post "/email_to_friend/:type/:id" => 'email_sender#send_mail', as: :email_to_friend
+  match "/email_to_friend/:type/:id" => 'email_sender#send_mail', as: :email_to_friend, via: [:get, :post]
 
   namespace :admin do
     resource :captcha_settings
