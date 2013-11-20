c9751120102aecfca18c00c154d9dff7836cc5d8
Ryan Bigg <radarlistener@gmail.com>
2013-11-21 09:32:51 +1100

    Fix possible routes issues caused by double drawing
    Conflicts:
    	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
-Spree::Core::Engine.routes.prepend do
-
+routes = lambda do
   match "/email_to_friend/:type/:id" => 'email_sender#send_mail', as: :email_to_friend, via: [:get, :post]
 
   namespace :admin do
     resource :captcha_settings
   end
+end
+
+if Spree::Core::Engine.respond_to?(:add_routes)
+  Spree::Core::Engine.add_routes(&routes)
+else
+  Spree::Core::Engine.routes.draw(&routes)
 end
