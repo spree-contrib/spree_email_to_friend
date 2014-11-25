3470d3c72b8152d35b3903cafc0b13662861f5ac
Tobias Bohwalli <hi@futhr.io>
2014-11-25 06:07:28 +0100

    Semantic versioning.

---
+++ lib/spree_email_to_friend/version.rb
@@ -0,0 +1,17 @@
+module SpreeEmailToFriend
+
+  # Returns the version of the currently loaded SpreeEmailToFriend as a
+  # <tt>Gem::Version</tt>.
+  def self.version
+    Gem::Version.new VERSION::STRING
+  end
+
+  module VERSION
+    MAJOR = 1
+    MINOR = 3
+    TINY  = 0
+    PRE   = nil
+
+    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+  end
+end
