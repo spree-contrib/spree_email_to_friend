959e5b277f6c71101bcbead7432a74fdb65e310c
Tobias Bohwalli <hi@futhr.io>
2015-01-13 13:37:07 +0100

    Update admin layout for Spree v3.0.0.beta

--- spec/features/admin/captcha_settings_spec.rb
+++ spec/features/admin/captcha_settings_spec.rb
@@ -1,22 +1,19 @@
-RSpec.feature 'Admin Captcha Settings', :js, type: :feature do
+RSpec.feature 'Admin Captcha Settings', :js do
   stub_authorization!
 
   context 'when update' do
     background 'visits admin pannel' do
-      visit spree.admin_path
-      click_link 'Configuration'
+      visit spree.edit_admin_captcha_settings_path
     end
 
     scenario 'display Captcha Settings on front end' do
-      expect(page).to have_text 'Captcha Settings'.upcase
-      click_link 'Captcha Settings'
+      expect(page).to have_text 'Captcha Settings'
       choose 'Yes'
       click_button 'Update'
       expect(find_field('Yes').value).not_to be_blank
     end
 
     scenario 'does not display Captcha Settings on frontend' do
-      click_link 'Captcha Settings'
       choose 'No'
       click_button 'Update'
       expect(find_field('No').value).not_to be_blank
