0463caf9348de6272066959c358dc01fc7bce901
Tobias Bohwalli <hi@futhr.io>
2015-08-16 13:12:13 +0200

    Update dependencies

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,12 +2,6 @@ require 'capybara/rspec'
 require 'capybara/rails'
 require 'capybara/poltergeist'
 
-RSpec.configure do |config|
+RSpec.configure do
   Capybara.javascript_driver = :poltergeist
-
-  config.before(:each, :js) do
-    if Capybara.javascript_driver == :selenium
-      page.driver.browser.manage.window.maximize
-    end
-  end
 end
