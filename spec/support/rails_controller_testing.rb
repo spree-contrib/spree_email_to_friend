4eb24517aec85b49e03c0d43613989734932a056
jimi <lukasz.golebiowski@icloud.com>
2016-10-26 17:04:40 +0200

    adjust for 3.2 beta

---
+++ spec/support/rails_controller_testing.rb
@@ -0,0 +1,9 @@
+require 'rails-controller-testing'
+
+RSpec.configure do |config|
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, :type => type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+    config.include ::Rails::Controller::Testing::Integration, :type => type
+  end
+end
