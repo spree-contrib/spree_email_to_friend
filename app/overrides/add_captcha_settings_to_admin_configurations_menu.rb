ee7b5ce44112e3b8d6a55ac4c5412a09059bf9c3
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2018-09-25 16:00:32 +0200

    Update classes to Bootstrap 4

--- app/overrides/add_captcha_settings_to_admin_configurations_menu.rb
+++ app/overrides/add_captcha_settings_to_admin_configurations_menu.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-  virtual_path: 'spree/admin/shared/_configuration_menu',
+  virtual_path: 'spree/admin/shared/sub_menu/_configuration',
   name: 'add_captcha_admin_option',
   insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
   text: '<%= configurations_sidebar_menu_item Spree.t(:captcha_settings, scope: :captcha), edit_admin_captcha_settings_path %>'
